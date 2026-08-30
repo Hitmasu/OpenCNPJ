36c6361308f9c4771b1c836beb4ebd271b128591
Rodrigo de Oliveira <rodrigodotnet@gmail.com>
2026-08-30 08:45:30 -0300

    test: cover selective upload verification

--- src/ETL/Tests/RcloneClientTests.cs
+++ src/ETL/Tests/RcloneClientTests.cs
@@ -39,6 +39,81 @@ public class RcloneClientTests
     }
 
     [TestMethod]
+    public void BuildRemoteMd5SumArguments_KeepsBroadFilterValidation()
+    {
+        var command = RcloneClient.BuildRemoteMd5SumArgumentsForTest(
+            "Opencnpj:opencnpj/files/shards/releases/abc",
+            ["*.ndjson", "*.index.bin"]);
+
+        Assert.AreEqual(
+            "md5sum \"Opencnpj:opencnpj/files/shards/releases/abc\" --filter \"+ *.ndjson\" --filter \"+ *.index.bin\" --filter \"- **\" ",
+            command);
+    }
+
+    [TestMethod]
+    public void BuildSelectedRemoteMd5SumArguments_UsesFilesFromRaw()
+    {
+        var command = RcloneClient.BuildSelectedRemoteMd5SumArgumentsForTest(
+            "Opencnpj:opencnpj/files/shards/releases/abc",
+            "/tmp/selected files.txt");
+
+        Assert.AreEqual(
+            "md5sum \"Opencnpj:opencnpj/files/shards/releases/abc\" --files-from-raw \"/tmp/selected files.txt\" ",
+            command);
+    }
+
+    [TestMethod]
+    public void BuildSelectedRemoteMd5SumArguments_DoesNotUseBroadExtensionFilters()
+    {
+        var command = RcloneClient.BuildSelectedRemoteMd5SumArgumentsForTest(
+            "Opencnpj:opencnpj/files/shards/releases/abc",
+            "/tmp/selected.txt");
+
+        Assert.IsFalse(command.Contains("--filter", StringComparison.Ordinal));
+        Assert.IsFalse(command.Contains("*.ndjson", StringComparison.Ordinal));
+        Assert.IsFalse(command.Contains("unselected.ndjson", StringComparison.Ordinal));
+    }
+
+    [TestMethod]
+    public void BuildSelectedUploadFiles_PreservesRelativePaths()
+    {
+        var files = RcloneClient.BuildSelectedUploadFilesForTest(
+            [
+                "z file.ndjson",
+                "sub dir/a.index.bin",
+                "routing/607.routing.bin"
+            ]);
+
+        CollectionAssert.AreEqual(
+            new[]
+            {
+                "routing/607.routing.bin",
+                "sub dir/a.index.bin",
+                "z file.ndjson"
+            },
+            files.ToArray());
+    }
+
+    [TestMethod]
+    public void BuildSelectedUploadFiles_ReturnsEmpty_WhenNoFilesWereSelected()
+    {
+        var files = RcloneClient.BuildSelectedUploadFilesForTest([]);
+
+        Assert.AreEqual(0, files.Count);
+    }
+
+    [TestMethod]
+    public async Task UploadSelectedFilesAsync_ReturnsTrue_WhenNoFilesWereSelected()
+    {
+        var uploaded = await RcloneClient.UploadSelectedFilesAsync(
+            "unused-local-folder",
+            "unused-remote-folder",
+            []);
+
+        Assert.IsTrue(uploaded);
+    }
+
+    [TestMethod]
     public void NormalizeBufferSize_UsesSafeDefault_WhenMissing()
     {
         Assert.AreEqual("16M", RcloneClient.NormalizeBufferSizeForTest(null));
